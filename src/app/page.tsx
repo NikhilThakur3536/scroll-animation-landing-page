1b22a8b2ce832b5211a15946363cba3ba60211ae
NikhilThakur3536 <thakurnikhil657@gmail.com>
2025-04-17 00:31:33 +0530

    Horizontal and vertical scalling added on scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@
 import {motion,useScroll,useTransform,useMotionValueEvent} from 'framer-motion';
 import { useRef } from 'react';
 
-
-
 export default function Home() {
   
   const scrollRef = useRef(null);
@@ -18,37 +16,39 @@ export default function Home() {
   });
 
   const topbottomscaleY = useTransform(scrollYProgress,[0,0.33],["5%","15%"])
-  // const horizontalscaleX = useTransform(scrollYProgress,[0,0.33],["5%","15%"])
+  const horizontalscaleX = useTransform(scrollYProgress,[0,0.33],["33%"," 36.5%"])
+  const horizontalscaleX2 = useTransform(scrollYProgress,[0,0.33],["66%"," 62.5%"])
+  const verticalscalling = useTransform(scrollYProgress,[0,0.36],["33%","39%"]) 
+  const verticalscalling2 = useTransform(scrollYProgress,[0,0.36],["66%","62%"]) 
   return (
-    <div ref={scrollRef} className=" relative w-screen h-[300vh] bg-white overflow-scroll hide-scrollbar">
+    <div ref={scrollRef} className=" relative w-screen h-[300vh] bg-white">
       <motion.div className="fixed absolute  left-0 w-full h-[0.5px] bg-[#C2D9FF] pointer-events-none"
       style={{top:topbottomscaleY}} />
       <div className="fixed absolute left-[44%] w-[1px] h-[5%] bg-[#C2D9FF] pointer-events-none" />
       <div className="fixed absolute left-[55%] w-[1px] h-[5%] bg-[#C2D9FF] pointer-events-none" />
-      <div className="fixed absolute left-[33%] w-[1px] h-full bg-[#C2D9FF] pointer-events-none" />
-      <div className="fixed absolute left-[66%] w-[1px] h-full bg-[#C2D9FF] pointer-events-none" />
-      <div className="fixed absolute top-[33%] left-0 w-[33%] h-[1px] bg-[#C2D9FF] pointer-events-none" />
-      <div className="fixed absolute top-[66%] left-0 w-[33%] h-[1px] bg-[#C2D9FF] pointer-events-none" />
+      <motion.div className="fixed absolute left-[33%] w-[1px] h-full bg-[#C2D9FF] pointer-events-none" 
+        style={{left:horizontalscaleX}}      
+      />
+      <motion.div className="fixed absolute left-[66%] w-[1px] h-full bg-[#C2D9FF] pointer-events-none" 
+        style={{left:horizontalscaleX2}}
+      />
+      <motion.div className="fixed absolute top-[33%] left-0 w-[33%] h-[1px] bg-[#C2D9FF] pointer-events-none" 
+        style={{width:horizontalscaleX, top:verticalscalling}}      
+      />
+      <motion.div className="fixed absolute top-[66%] left-0 w-[33%] h-[1px] bg-[#C2D9FF] pointer-events-none"
+        style={{width:horizontalscaleX,top:verticalscalling2}} 
+      />
       <motion.div className="fixed absolute  left-0 w-full h-[0.5px] bg-[#C2D9FF] pointer-events-none"
-          style={{bottom:topbottomscaleY}}
-        />
-      <div className="fixed absolute top-[33%] left-[66%] w-[34%] h-[1px] bg-[#C2D9FF] pointer-events-none" />
-      <div className="fixed absolute top-[66%] left-[66%] w-[34%] h-[1px] bg-[#C2D9FF] pointer-events-none" />
-      <div className='fixed absolute top-[5%] w-[33%] h-[90%] bg-rose-200 left-[33%]'>
-
-      </div>
+        style={{bottom:topbottomscaleY}}
+      />
+      <motion.div className="fixed absolute top-[33%] left-[66%] w-[40%] h-[1px] bg-[#C2D9FF] pointer-events-none" 
+        style={{left:horizontalscaleX2, top:verticalscalling}}      
+      />
+      <motion.div className="fixed absolute top-[66%] left-[66%] w-[40%] h-[1px] bg-[#C2D9FF] pointer-events-none" 
+        style={{left:horizontalscaleX2,top:verticalscalling2}}
+      />
+      {/* <div className='fixed absolute top-[5%] w-[33%] h-[90%] bg-rose-200 left-[33%]'>
+      </div> */}
     </div>
   )
 }  
-
-
-
-// const scrollRef = useRef(null);
-// const { scrollYProgress } = useScroll({
-//   target: scrollRef,
-//   offset: ["start start", "end start"],
-// });
-// // `
-// //   const scale = useTransform(scrollYProgress, [0, 1], [1.1, 0.5]);`
-// const scaleX= useTransform(scrollYProgress,[0,1],[1.5,0.5 ])
-// const scaleY= useTransform(scrollYProgress,[0,1],[1.5,0.5])
